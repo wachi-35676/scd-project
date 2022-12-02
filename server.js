9dc52962e4a04146bee7520ddc78e4b63ef2b88c
Muhammad Wasif Ali Wasif <76695742+wachi-35676@users.noreply.github.com>
2022-12-02 15:28:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use('/exercises', exercises);
 app.use('/routines', routines);
 app.use('/bodyMeasurements', bodyMeasurements);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
